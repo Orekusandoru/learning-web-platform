059af8bbb0c5315e3c456666967df30d5e855bc9
_SaKe_ <sasha.g360@gmail.com>
2024-04-30 22:44:21 +0300

    added tailwind and config

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from backend.models import Profile,User
+from backend.serializer import MyTokenObtainPairSerializer, UserSerializer
+
+from rest_framework.decorators import api_view
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
